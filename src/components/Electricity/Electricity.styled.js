3c87441b2ffd9f0b038be53727b733bb30a3e820
Ruslan Karpov <rkarpov88@gmail.com>
2023-12-03 23:28:19 +0200

    fix padding Electricity

--- src/components/Electricity/Electricity.styled.js
+++ src/components/Electricity/Electricity.styled.js
@@ -9,9 +9,11 @@ export const Wrapper = styled.section`
   padding-bottom: 36px;
   @media screen and (min-width: 768px) {
     padding-bottom: 100px;
+    padding-top: 100px;
   }
   @media screen and (min-width: 1280px) {
     padding-bottom: 120px;
+    padding-top: 120px;
   }
 `;
 
